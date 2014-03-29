364cb454c0af23f2abb10f64d5110b941bba9ff9
alex <alex@alex-HP-Pavilion-dv5-Notebook-PC.(none)>
2014-03-29 11:58:07 +0400

    :: true ->

--- lab3/wikiparser.py
+++ lab3/wikiparser.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import ipdb
 import xml.sax
 
 import mwparserfromhell
@@ -18,6 +19,7 @@ class WikiContentHandler(xml.sax.ContentHandler):
         self.depth = 0
         self.text = None
         self.mband = None
+        self.count = 0
 
     def startElement(self, name, attrs):
         if name == 'page':
@@ -54,27 +56,151 @@ class WikiContentHandler(xml.sax.ContentHandler):
                 self.parse()
 
     def parse(self):
-        print self.text
-        import ipdb; ipdb.set_trace()
-        names = (u'Название', u'Страна', u'Годы', u'Город', u'Состав',
-                 u'Бывшие участники', u'Жанры', u'Язык')
+        if self.count > 500:
+            raise Exception('Limit exceeded')
+        self.count = self.count + 1
+        print '==========================================='
+        print 'count: ', self.count
+        # print 'text:'
+        # print self.text
+        data = {}
+
+        names = (u'Название', u'Страна', u'Страны', u'Годы', u'Город',
+                 u'Состав', u'Бывшие участники', u'Жанры', u'Язык')
         wikicode = mwparserfromhell.parse(self.text)
         templates = wikicode.filter_templates()
+        if len(templates) == 0:
+            return
         template = templates[0]
+        print '++++++++++++++++++++++++++++++++++++++++++++'
         for param in template.params:
             name = unicode(param.name).strip()
             if name not in names:
                 continue
 
             if name == u'Название':
-                ipdb; ipdb.set_trace()
-                band_name = unicode(param.value).strip()
-            else:
-                print 'name:', name
-                ipdb.set_trace()
-                print 'value:', param.value
-        ipdb.set_trace()
-        print res
+                # ipdb; ipdb.set_trace()
+                data['name'] = unicode(param.value).strip()
+                print data['name']
+            elif name == u'Страна':
+                if self.count in (358, ):
+                    ipdb.set_trace()
+
+                if not unicode(param.value).strip():
+                    continue
+                strs = unicode(param.value)
+                strs = [s.strip() for s in strs.split(',')]
+                data['countries'] = list(set([s for s in strs if s]))
+                print 'unicode: ', unicode(param.value)
+                # print 'type: ', type(param.value)
+                # print 'unparsed texts:'
+                # for p in param.value.filter_text():
+                #     print p
+                print 'parsed:'
+                print data['countries'][0]
+            elif name == u'Страны':
+                if self.count in (358, 338, ):
+                    ipdb.set_trace()
+
+                print 'unicode: ', unicode(param.value)
+                # print 'type: ', type(param.value)
+                # print 'unparsed texts:'
+                # for p in param.value.filter_text():
+                #     print p
+
+                def vlog():
+                    print 'parsed:'
+                    for p in data['countries']:
+                        print p
+
+
+                tmpls = [tmpl for tmpl in param.value.filter_templates()]
+                if tmpls:
+                    def get_country(tmpl):
+                        if len(tmpl.params) == 0:
+                            return unicode(tmpl.name)
+                        return unicode(tmpl.params[0])
+                    data['countries'] = map(get_country, tmpls)
+                    vlog()
+                    continue
+
+                links = [link for link in param.value.filter_wikilinks()]
+                if links:
+                    def get_country(link):
+                        if link.text:
+                            return unicode(link.text)
+                        return unicode(link.title)
+                    data['countries'] = map(get_country, links)
+                    vlog()
+                    continue
+
+                # if self.count == 197:
+                #     ipdb.set_trace()
+
+                tags = [unicode(tg.tag) for tg in param.value.filter_tags()]
+                strs = [unicode(s) for s in param.value.filter_text()]
+                names = []
+                for s in strs:
+                    names.extend(s.split(','))
+                names = [s.strip() for s in names]
+                data['countries'] = list(set([s for s in names if s]))
+                vlog()
+            # else:
+            #     # print 'name:', name
+            #     # # ipdb.set_trace()
+            #     # print 'value:', param.value
+            #     pass
+        # print data
+        # ipdb.set_trace()
+
+    def parse_country(self, param, verbose=False):
+        def vlog():
+            if not verbose:
+                return
+
+            print 'parsed countries:'
+            for p in cntrs:
+                print 'country: "{}"'.format(p)
+
+        cntrs = []
+
+        if not unicode(param.value).strip():
+            # value is empty
+            vlog()
+            return cntrs
+
+        tmpls = [tmpl for tmpl in param.value.filter_templates()]
+        if tmpls:
+            def get_country(tmpl):
+                if len(tmpl.params) == 0:
+                    return unicode(tmpl.name)
+                return unicode(tmpl.params[0])
+            
+            cntrs = map(get_country, tmpls)
+            vlog()
+            return cntrs
+
+        links = [link for link in param.value.filter_wikilinks()]
+        if links:
+            def get_country(link):
+                if link.text:
+                    return unicode(link.text)
+                return unicode(link.title)
+
+            cntrs = map(get_country, links)
+            vlog()
+            return cntrs
+
+        tags = [unicode(tg.tag) for tg in param.value.filter_tags()]
+        strs = [unicode(s) for s in param.value.filter_text()]
+        names = []
+        for s in strs:
+            names.extend(s.split(','))
+        names = [s.strip() for s in names]
+        
+        cntrs = list(set([s for s in names if s]))
+        vlog()
+        return cntrs
 
 
 def parse(filename):
